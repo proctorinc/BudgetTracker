ca417c69f946f61c2f9a0df8681c048a7fd5e144
Matt Proctor <proctormatt0@gmail.com>
2022-12-15 07:46:17 -0800

    Fixed AccountsTotalBalance to pass tests

--- src/features/accounts/components/AccountsTotalBalance.jsx
+++ src/features/accounts/components/AccountsTotalBalance.jsx
@@ -2,11 +2,13 @@ import { formatCurrency } from "@/utils/currency";
 import { useAccounts } from "../hooks/useAccounts";
 
 const AccountsTotalBalance = () => {
-  const { netBalance } = useAccounts();
+  const { data } = useAccounts();
 
   return (
     <div className="flex justify-center p-4">
-      <h1 className="text-3xl">Net Worth: {formatCurrency(netBalance)}</h1>
+      <h1 className="text-3xl">
+        Net Worth: {formatCurrency(data?.total_balance)}
+      </h1>
     </div>
   );
 };
