9a4fecc8233a54c28c78e9f7dfa25de457f78d30
Matt Proctor <mproctor@securecodewarrior.com>
2023-02-02 14:54:17 -0800

    Fixed profile tests

--- src/features/users/routes/__tests__/Profile.test.jsx
+++ src/features/users/routes/__tests__/Profile.test.jsx
@@ -1,12 +1,19 @@
-import { describe, it, vi } from "vitest";
-import { screen, render, userEvent, withAuth } from "@/testUtils.jsx";
+import { describe, it } from "vitest";
+import { screen, render, withAuth } from "@/testUtils.jsx";
 import Profile from "../Profile";
+import { mockUser } from "@/__mocks__/mock_features/auth";
 
 describe("Profile Route", () => {
   it("renders profile", async () => {
     render(withAuth(<Profile />));
 
-    const profileTitle = await screen.findByRole("heading", /profile/i);
+    const profileTitle = await screen.findByRole("heading", {
+      name: /profile/i
+    });
+    const emailHeader = await screen.findByRole("heading", {
+      name: /email:/i
+    });
     expect(profileTitle).toBeInTheDocument();
+    expect(emailHeader).toHaveTextContent(mockUser.email)
   });
 });
